ce61b7940f3f25f132a9ed91957ed8635d1b88cc
SeoungJin Kim <dge03078@naver.com>
2017-10-18 03:31:44 +0900

    xml/update activity_register_additionalinfo and RegisterAdditionalActivity

--- app/src/main/java/com/example/administrator/ifindyou/RegisterActivity.java
+++ app/src/main/java/com/example/administrator/ifindyou/RegisterActivity.java
@@ -1,16 +1,37 @@
 package com.example.administrator.ifindyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2017-10-18.
  */
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvAdditionalRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tvAdditionalRegister = (TextView) findViewById(R.id.register_button);
+        tvAdditionalRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.register_button:
+                Intent intent = new Intent(this, RegisterAdditionalActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/ifindyou/RegisterAdditionalActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.ifindyou;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017-10-18.
+ */
+
+public class RegisterAdditionalActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_additionalinfo);
+    }
+}
