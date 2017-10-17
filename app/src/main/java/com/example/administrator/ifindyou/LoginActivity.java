90a494e4f0163289908deb71a38842c375904298
SeoungJin Kim <dge03078@naver.com>
2017-10-18 01:39:31 +0900

    add strings.xml, colors.xml and fix activity_login.xml

--- app/src/main/java/com/example/administrator/ifindyou/LoginActivity.java
+++ app/src/main/java/com/example/administrator/ifindyou/LoginActivity.java
@@ -1,8 +1,17 @@
 package com.example.administrator.ifindyou;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Administrator on 2017-10-18.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
