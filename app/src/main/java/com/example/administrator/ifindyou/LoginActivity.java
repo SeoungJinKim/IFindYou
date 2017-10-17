d0e27202a3554825f9e9c647a3e52e4d633f39de
SeoungJin Kim <dge03078@naver.com>
2017-10-18 02:18:43 +0900

    xml/update activity_register, strings and RegisterActivity and add animation

--- app/src/main/java/com/example/administrator/ifindyou/LoginActivity.java
+++ app/src/main/java/com/example/administrator/ifindyou/LoginActivity.java
@@ -1,17 +1,42 @@
 package com.example.administrator.ifindyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2017-10-18.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView textAnotherRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textAnotherRegister = (TextView) findViewById(R.id.another_regist);
+        textAnotherRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.another_regist:
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+                break;
+        }
+    }
+
+    public void onBackPressed() {
+        super.finish();
+        overridePendingTransition(R.anim.anim_exit_in, R.anim.anim_exit_out);
     }
 }
