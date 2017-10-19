bb394b51f6e8843cb5e5bd42614271968b03b6cd
SeoungJinKim <dge03078@naver.com>
2017-10-19 18:10:22 +0900

    feature/develop search_base

--- app/src/main/java/com/example/administrator/ifindyou/MainActivity.java
+++ app/src/main/java/com/example/administrator/ifindyou/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.ifindyou;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -8,9 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -20,12 +28,15 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener {
 
     private ImageButton iconSetting, iconNoti;
     private SharedPreferences pref;
     private ArrayList<User> userList;
     private ListView userListView;
+    private EditText searchName;
+    private Button searchButton;
+    private Spinner chooseUnit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +50,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pref = getSharedPreferences("PrefIFindYou", Activity.MODE_PRIVATE);
 
         iconSetting = (ImageButton) findViewById(R.id.btn_nav_setting);
-        iconSetting.setOnClickListener(this);
         iconNoti = (ImageButton) findViewById(R.id.btn_nav_notification);
+        searchName = (EditText) findViewById(R.id.search_name);
+        searchButton = (Button) findViewById(R.id.search_button);
+        chooseUnit = (Spinner) findViewById(R.id.choose_unit);
+
+        iconSetting.setOnClickListener(this);
         iconNoti.setOnClickListener(this);
+        searchName.setOnEditorActionListener(this);
+        searchButton.setOnClickListener(this);
 
         userListView = (ListView) findViewById(R.id.custom_list_listView);
-        refreshData();
+        refreshData(0);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent e) {
+        if (actionId == EditorInfo.IME_ACTION_DONE && v.getId() == R.id.search_name) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(searchName.getWindowToken(), 0);
+            searchName.clearFocus();
+        }
+        return true;
     }
 
     @Override
@@ -64,6 +91,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.search_button:
+                refreshData(1);
+                break;
         }
     }
 
@@ -77,29 +107,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         userListView.setAdapter(customAdapter);
     }
 
-    private void refreshData() {
+    private void refreshData(int check) {
         RequestParams params = new RequestParams();
 
-        params.put("Id", pref.getString("User_Id", ""));
+        if (check == 0) {
+            params.put("Id", pref.getString("User_Id", ""));
 
-        client.get("http://10.53.128.156:5013/loadStarData", params, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                Log.d("data", "DataLoadSuccess " + responseBody + " statuscode" + statusCode);
-                // SAVE SESSION_KEY - PREF
+            client.get("http://10.53.128.156:5013/loadStarData", params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    Log.d("data", "DataLoadSuccess " + responseBody + " statuscode" + statusCode);
+                    // SAVE SESSION_KEY - PREF
 
-                // null 처리 해줄것
-                if(statusCode == 201) {
-                    String jsonData = new String(responseBody);
+                    // null 처리 해줄것
+                    if (statusCode == 201) {
+                        String jsonData = new String(responseBody);
 
-                    listView(jsonData);
+                        listView(jsonData);
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Log.d("login_attempt", "attempt: " + statusCode);
-            }
-        });
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Log.d("login_attempt", "attempt: " + statusCode);
+                }
+            });
+        }
+        else
+        {
+            params.put("Name",searchName.getText().toString());
+            params.put("Unit",chooseUnit.getSelectedItem().toString());
+            client.get("http://10.53.128.156:5013/loadSearchData", params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    Log.d("data", "DataLoadSuccess " + responseBody + " statuscode" + statusCode);
+                    // SAVE SESSION_KEY - PREF
+
+                    // null 처리 해줄것
+                    if (statusCode == 201) {
+                        String jsonData = new String(responseBody);
+
+                        listView(jsonData);
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Log.d("login_attempt", "attempt: " + statusCode);
+                }
+            });
+        }
     }
 }
